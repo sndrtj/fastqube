1f9d996f743cf753f900a299813f9a8f4ee8d0ad
Sander Bollen <sander@sndrtj.eu>
2019-04-12 23:14:06 +0200

    prototype compressor

--- prototype.py
+++ prototype.py
@@ -6,9 +6,39 @@ fastqube prototype
 :license: BSD-3-clause
 """
 import argparse
+import datetime
 import pathlib
+import sys
 from typing import NamedTuple, List
 import bitstruct
+
+
+SEQ_MAP = {
+    "A": 0,
+    "T": 1,
+    "C": 2,
+    "G": 3,
+    "N": 4
+}
+
+SEQ_MAP_REV = {v: k for k, v in SEQ_MAP.items()}
+
+
+# bistruct header format
+HEADER_FMT = "t32768"
+
+
+# bitstruct format for 150-base read
+READ_FMT = "t512" + ("u3"*150) + ("u6"*150)
+
+
+def create_read_fmt(read_id: str) -> str:
+    """create a bitstruct format for any given read it up to 512 bits"""
+    if len(read_id) > 64:
+        raise ValueError("Read ID is too large")
+    padding = 512 - len(read_id)*8
+    r_fmt = "t{0}p{1}".format(len(read_id)*8, padding)
+    return r_fmt + ("u3" * 150) + ("u6" * 150)
 
 
 class ParsedFastqRead(NamedTuple):
@@ -17,14 +47,24 @@ class ParsedFastqRead(NamedTuple):
     qualities: List[int]
 
     def serialize(self) -> 'RawFastqRead':
-        pass
+        return RawFastqRead(
+            read_id=self.read_id,
+            seq=[SEQ_MAP_REV[x] for x in self.seq],
+            qualities=[chr(i+32) for i in self.qualities]
+        )
 
     def compress(self) -> bytearray:
-        pass
+        fmt = create_read_fmt(self.read_id)
+        args = [self.read_id] + self.seq + self.qualities
+        return bitstruct.pack(fmt, *args)
 
     @classmethod
     def from_bytearray(cls, bytearray) -> 'ParsedFastqRead':
-        pass
+        unpacked = bitstruct.unpack(READ_FMT, bytearray)
+        read_id = unpacked[0]
+        seq = unpacked[1: 151]
+        qual = unpacked[151:]
+        return cls(read_id, seq, qual)
 
 
 class RawFastqRead(NamedTuple):
@@ -33,7 +73,11 @@ class RawFastqRead(NamedTuple):
     qualities: List[str]
 
     def deserialize(self) -> ParsedFastqRead:
-        pass
+        return ParsedFastqRead(
+            read_id=self.read_id,
+            seq=[SEQ_MAP[x.upper()] for x in self.seq],
+            qualities=[ord(x)-32 for x in self.qualities]
+        )
 
 
 class RawRastqReader(object):
@@ -66,6 +110,16 @@ class RawRastqReader(object):
         self.handle.close()
 
 
+def create_header() -> bytearray:
+    val = ("program: fastqube \n"
+           "version: PROTOTYPE \n"
+           "mode: LOSSLESS\n"
+           "date: {0} \n").format(datetime.datetime.utcnow().isoformat())
+    padding = (4096*8) - (len(val)*8)
+    fmt = "t{0}p{1}".format(len(val)*8, padding)
+    return bitstruct.pack(fmt, val)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=pathlib.Path)
@@ -76,8 +130,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.compress:
-        raise NotImplementedError
+        sys.stdout.buffer.write(create_header())
+        reader = RawRastqReader(args.input)
+        for read in reader:
+            compressed = read.deserialize().compress()
+            sys.stdout.buffer.write(compressed)
+        reader.close()
     elif args.decompress:
         raise NotImplementedError
     else:
         raise NotImplementedError
+    sys.stdout.buffer.flush()
