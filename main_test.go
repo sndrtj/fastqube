21cb9468fe48b985ef111eb7894303a651ac2c25
Sander Bollen <sander@sndrtj.eu>
2019-05-05 17:58:30 +0200

    add more tests

--- main_test.go
+++ main_test.go
@@ -20,10 +20,31 @@ func TestQualityDecoding(t *testing.T) {
 		want []int
 	}{
 		{"JJJJJ", []int{41, 41, 41, 41, 41}},
+		{"!J!J!J!", []int{0, 41, 0, 41, 0, 41, 0}},
 	}
 
 	for _, c := range cases {
 		got := decodeQualitryString(c.s)
+		if !intSliceEqual(got, c.want) {
+			t.Errorf("Wanted %q, got %q", c.want, got)
+		}
+	}
+}
+
+func TestSeqStringAsIntValid(t *testing.T) {
+	cases := []struct {
+		s    string
+		want []int
+	}{
+		{"AAAAA", []int{0, 0, 0, 0, 0}},
+		{"ACTGN", []int{0, 2, 1, 3, 4}},
+	}
+
+	for _, c := range cases {
+		got, err := seqStringToInts(c.s)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
 		if !intSliceEqual(got, c.want) {
 			t.Errorf("Wanted %q, got %q", c.want, got)
 		}
